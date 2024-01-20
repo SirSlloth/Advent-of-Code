539e3eb862e4f0bd61555383ebdef997a9f379bc
Charles Woodcock <charleswoodcock@Charless-MacBook-Air.local>
2024-01-20 16:07:04 -0500

    Finished day 2 solution

--- 2015/day_02/day_02.py
+++ 2015/day_02/day_02.py
@@ -1,16 +1,19 @@
+import os
+
 def main():
     ans1 = 0
     ans2 = 0 
-    with open("input_day_02.txt", mode = "r") as i:
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/input_day_02.txt", mode = "r") as i:
         lines = i.readlines()
     for l in lines:
         d1, d2, d3 = map(int, l.split("x"))
         s = [(d1*d2), (d2*d3), (d1*d3)]
-        p = [()]
+        p = [(d1+d2)*2,(d1+d3)*2,(d2+d3)*2]
         t = 2*(s[0] + s[1] + s[2]) + (min(s))
+        r = min(p) + (d1*d2*d3)
         ans1 += t
+        ans2 += r
     print(f"The elves need to order {ans1} feet of wrapping paper.")
-
     print(f"The elves need to order {ans2} feet of ribbon.")
     return
 
